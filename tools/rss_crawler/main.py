e19e1eac984033d1b546876370da0d2b83a334bb
Manbok Lee <manbok.lee.readmoa@gmail.com>
2020-12-26 21:03:28 -0800

    Add a logic to fetch and parse RSS document.

--- tools/rss_crawler/main.py
+++ tools/rss_crawler/main.py
@@ -1,26 +1,91 @@
 from bs4 import BeautifulSoup
+import logging
+import os
+import requests
 
-xml_doc = """
-<feed xmlns="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
-<author>
-<name>Blog lists</name>
-</author>
-<id>좋은 블로그</id>
-<title>Korea Blogs</title>
-<updated>2020-12-26T00:30:00+09:00</updated>
-<entry>
-<author>
-<name>홍길동</name>
-</author>
-<id>https://readmoa.net/foo</id>
-<link href="https://readmoa.net/foo"/>
-<summary><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd"> <html><body><p>테스트</p></body></html> </summary>
-<title>테스트 제목</title>
-<updated>2020-12-22T16:33:00+09:00</updated>
-<dc:date>2020-12-22T16:33:00+09:00</dc:date>
-</entry>
-"""
-# Requirement: pip install lxml
-soup = BeautifulSoup(xml_doc, "lxml")
+logger = logging.getLogger()
 
-print(soup.prettify())
+RSS_FEED_FILE = "feeds.txt"
+
+def read_feed_file(feed_filepath):
+    """Parses a feed list file and returns a list of feed URLs.
+
+    Args:
+      feed_filepath: A path to the file that contains a list of RSS feeds.
+
+    Returns:
+      A list of RSS feed URLs.
+    """
+    urls = []
+
+    with open(feed_filepath) as f:
+        content = f.readlines()
+
+    for line in content:
+        if line[0] == '#':
+            continue
+        urls.append(line.strip())
+
+    return urls
+
+def fetch_rss(url):
+    """Fetch RSS document from the given URL.
+
+    Args:
+      url: URL for RSS XML document.
+
+    Returns:
+      The content of RSS document.
+    """
+    rss_doc = requests.get(url)
+    
+    if rss_doc.status_code != 200:
+        logger.warning(
+                "Failed to fetch with Response Code %d for %s",
+                rss_doc.status_code, rss_doc.url)
+
+    return rss_doc.content
+
+def read_rss(rss_content):
+    """Parses RSS XML content and returns a list of entries.
+
+    Parses an input RSS XML content and returns a list of content entries.
+
+    Args:
+      rss_content: A text string of RSS XML content.
+
+    Returns:
+      A list of dictionaries mapping attribute names to values.
+
+      [{'title': 'foo', 'link': 'https://example.com/'},
+       {'title': 'bar', 'link': 'https://example.com/another'}
+      ]
+    """
+    soup = BeautifulSoup(rss_content, "lxml")
+    
+    content_list = []
+    num_entries = 0
+    for i in soup.find_all("entry"):
+        num_entries += 1
+        content_list.append({
+            "title": i.find("title").get_text(),
+            "updated": i.find("updated").get_text(),
+            "link": i.find("link")["href"],
+            "author": i.find("author").find("name").get_text(),
+            "summary": i.find("summary").get_text()
+            })
+    
+    logger.info("Number of entries: %d", len(content_list));
+    return content_list
+
+def main():
+    feed_path = os.path.join(os.path.dirname(__file__), RSS_FEED_FILE)
+    for feed_url in read_feed_file(feed_path):
+        rss_content = fetch_rss(feed_url)
+        parsed_content = read_rss(rss_content)
+        for record in parsed_content:
+            print("Title: ", record["title"])
+    
+
+if __name__ == "__main__":
+    main()

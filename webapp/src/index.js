c138204677e611d0ce10bec943513f3497c2ba3b
Manbok Lee <manbok.lee.readmoa@gmail.com>
2021-02-07 19:37:19 -0800

    Add menu items for ReadMoa blog and the source code.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,16 +1,21 @@
 // @flow
-
 import React from "react";
 import ReactDOM from "react-dom";
 import AppBar from "@material-ui/core/AppBar";
+import BookIcon from "@material-ui/icons/Book";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CodeIcon from "@material-ui/icons/Code";
+import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import * as serviceWorker from "./serviceWorker";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
+import { Typography } from "@material-ui/core";
+import * as serviceWorker from "./serviceWorker";
 
 // Custom resources.
 import "./index.css";
@@ -18,7 +23,6 @@ import ListPosts from "./listPosts";
 import ListPostsByAuthor from "./listPostsByAuthor";
 import WritePost from "./writePost";
 import ViewPost from "./viewPost";
-import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,12 +34,28 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  menuItem: {
+    width: "100%",
+    maxWidth: 360,
+    // TODO: choose the right color (palette.background.paper).
+    backgroundColor: theme.palette.action.disabledBackground,
+    color: theme.palette.text.secondary,
+  },
 }));
 
 function MainApp() {
   const classes = useStyles();
 
   const root_elem = document.getElementById("root");
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Router>
@@ -47,9 +67,32 @@ function MainApp() {
               className={classes.menuButton}
               color="inherit"
               aria-label="menu"
+              onClick={handleClick}
             >
               <MenuIcon />
             </IconButton>
+
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem button component="a" href="https://readmoa.github.io/">
+                <BookIcon /> 블로그
+              </MenuItem>
+              <Divider />
+              <MenuItem
+                button
+                component="a"
+                href="https://www.github.com/ReadMoa/web-service"
+              >
+                <CodeIcon />
+                소스 코드
+              </MenuItem>
+            </Menu>
+
             <Box
               display="flex"
               justifyContent="flex-start"
